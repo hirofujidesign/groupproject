240fadcb2e1564af7b8988305fe5a91ba46ca013
xirofuji <hirosfujita@gmail.com>
2016-03-26 14:48:00 -0400

    commit

--- js/init.js
+++ js/init.js
@@ -6,3 +6,9 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+// $(document).ready(function () {
+//     $(window).on('load scroll', function () {
+//         var scrolled = $(this).scrollTop();
+//         $('#video').css('transform', 'translate3d(0, ' + -(scrolled * 0.25) + 'px, 0)'); // parallax (25% scroll rate)
+//     });
