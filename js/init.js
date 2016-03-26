9c6a495bb48254ad76d1c6aec0638e1d2220be52
xirofuji <hirosfujita@gmail.com>
2016-03-26 19:22:48 -0400

    commit

--- js/init.js
+++ js/init.js
@@ -9,6 +9,6 @@
 
 // $(document).ready(function () {
 //     $(window).on('load scroll', function () {
-//         var scrolled = $(this).scrollTop();
+//         var scrolled = $("#video").scrollTop();
 //         $('#video').css('transform', 'translate3d(0, ' + -(scrolled * 0.25) + 'px, 0)'); // parallax (25% scroll rate)
 //     });
