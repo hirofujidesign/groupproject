e2ce0ffffbdf102e845931f9abc6d60ec51c8077
xirofuji <hirosfujita@gmail.com>
2016-03-31 18:32:09 -0400

    fix

--- js/init.js
+++ js/init.js
@@ -12,3 +12,22 @@
 //         var scrolled = $("#video").scrollTop();
 //         $('#video').css('transform', 'translate3d(0, ' + -(scrolled * 0.25) + 'px, 0)'); // parallax (25% scroll rate)
 //     });
+var myApp = angular.module('myApp', []);
+myApp.controller('mainCtrl', function ($scope, $http){
+  
+  $http.get('http://api.randomuser.me/?results=24').success(function(data) {
+    $scope.users = data.results;
+  }).error(function(data, status) {
+    alert('get data error!');
+  });
+  
+  $scope.removeUser = function(user){
+     $scope.users.splice($scope.users.indexOf(user),1);
+  };
+  
+  $scope.modalDetails = function(user){
+     $scope.user = user;
+     $('#modalDetails').openModal();
+  };
+  
+});
